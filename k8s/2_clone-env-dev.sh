75a01d049e6a077daf5059455d966ca8c0c20bc7
mrunal-modi <mrunal.modi@gmail.com>
2023-11-22 16:14:11 +1100

    actoolkit

--- k8s/2_clone-env-dev.sh
+++ k8s/2_clone-env-dev.sh
@@ -6,10 +6,10 @@ clone_env() {
     cloneNamespace=mern-app-dev
     ClusterName=rke1
     sourceAppName=mern-app-prd
-    clusterID=$(/home/user/.local/bin/actoolkit -o table list clusters | awk -v cn="$ClusterName" '$2==cn{print $4}')
-    sourceAppID=$(/home/user/.local/bin/actoolkit -o table list apps | awk -v sa="$sourceAppName" '$2==sa{print $4}')
+    clusterID=$(actoolkit -o table list clusters | awk -v cn="$ClusterName" '$2==cn{print $4}')
+    sourceAppID=$(actoolkit -o table list apps | awk -v sa="$sourceAppName" '$2==sa{print $4}')
     # Execute the command
-    /home/user/.local/bin/actoolkit clone --cloneAppName $cloneAppName --clusterID $clusterID --cloneNamespace $cloneNamespace --sourceAppID $sourceAppID || { echo "actoolkit clone command failed"; exit 1; }
+    actoolkit clone --cloneAppName $cloneAppName --clusterID $clusterID --cloneNamespace $cloneNamespace --sourceAppID $sourceAppID || { echo "actoolkit clone command failed"; exit 1; }
     kubectl delete deployment frontend -n $cloneNamespace || echo "Deployment frontend not found in namespace $cloneNamespace"
     kubectl delete service frontend-svc -n $cloneNamespace || echo "Service frontend-svc not found in namespace $cloneNamespace"
     kubectl apply -f frontend-deployment-dev.yaml || echo "Failed to apply frontend-deployment-dev.yaml"
@@ -21,9 +21,9 @@ clone_env() {
 delete_env() {
     cloneAppName=mern-app-dev
     cloneNamespace=mern-app-dev
-    cloneAppID=$(/home/user/.local/bin/actoolkit -o table list apps | awk -v sa="$cloneAppName" '$2==sa{print $4}')
+    cloneAppID=$(actoolkit -o table list apps | awk -v sa="$cloneAppName" '$2==sa{print $4}')
     # Execute the commands
-    /home/user/.local/bin/actoolkit unmanage app $cloneAppID
+    actoolkit unmanage app $cloneAppID
     kubectl delete namespace $cloneNamespace
 }
 
