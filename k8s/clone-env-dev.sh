a74cb3cccb1481184625bd19a28211301488110a
mrunal-modi <mrunal.modi@gmail.com>
2023-11-22 14:54:31 +1100

    k8s clone

--- k8s/clone-env-dev.sh
+++ k8s/clone-env-dev.sh
@@ -1,17 +1,42 @@
+#!/bin/bash
 
-cloneAppName=mern-app-dev
-cloneNamespace=mern-app-dev
-clusterID=/home/user/.local/bin/actoolkit -o table list clusters | awk '$2=="rke1"{print $4}'
-sourceAppID=/home/user/.local/bin/actoolkit -o table list apps | awk '$2=="mern-app-prd"{print $4}'
+# Function to clone environment
+clone_env() {
+    cloneAppName=mern-app-dev
+    cloneNamespace=mern-app-dev
+    ClusterName=rke1
+    sourceAppName=mern-app-prd
+    clusterID=$(/home/user/.local/bin/actoolkit -o table list clusters | awk -v cn="$ClusterName" '$2==cn{print $4}')
+    sourceAppID=$(/home/user/.local/bin/actoolkit -o table list apps | awk -v sa="$sourceAppName" '$2==sa{print $4}')
+    # Execute the command
+    /home/user/.local/bin/actoolkit clone --cloneAppName $cloneAppName --clusterID $clusterID --cloneNamespace $cloneNamespace --sourceAppID $sourceAppID || { echo "actoolkit clone command failed"; exit 1; }
+    kubectl delete deployment frontend -n $cloneNamespace || echo "Deployment frontend not found in namespace $cloneNamespace"
+    kubectl delete service frontend-svc -n $cloneNamespace || echo "Service frontend-svc not found in namespace $cloneNamespace"
+    kubectl apply -f frontend-deployment-dev.yaml || echo "Failed to apply frontend-deployment-dev.yaml"
+    kubectl apply -f frontend-service-dev.yaml || echo "Failed to apply frontend-service-dev.yaml"
+    kubectl apply -f ingress-resource-dev.yaml || echo "Failed to apply ingress-resource-dev.yaml"
+}
 
-/home/user/.local/bin/actoolkit clone --cloneAppName cloneAppName \
---clusterID 601ff60e-1fcb-4f69-be89-2a2c4ca5a715 \
---cloneNamespace mern-app-dev \
---sourceAppID 0a6de1ad-afe8-4a60-9de3-385adcdae34d
+# Function to delete environment
+delete_env() {
+    cloneAppName=mern-app-dev
+    cloneNamespace=mern-app-dev
+    cloneAppID=$(/home/user/.local/bin/actoolkit -o table list apps | awk -v sa="$cloneAppName" '$2==sa{print $4}')
+    # Execute the commands
+    /home/user/.local/bin/actoolkit unmanage app $cloneAppID
+    kubectl delete namespace $cloneNamespace
+}
 
-kubectl delete deployment frontend -n mern-app-dev
-kubectl delete service frontend-svc -n mern-app-dev
-kubectl apply -f frontend-deployment-dev.yaml
-kubectl apply -f frontend-service-dev.yaml
-kubectl apply -f ingress-resource-dev.yaml
-# echo "192.168.0.215 dev.myexampleapp.com" >> /etc/hosts
+# Main script execution
+case $1 in
+    clone)
+        clone_env
+        ;;
+    delete)
+        delete_env
+        ;;
+    *)
+        echo "Usage: $0 [clone|delete]"
+        exit 1
+        ;;
+esac
--- k8s/deploy-env-dev.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete deployment frontend -n mern-app-dev
-kubectl delete service frontend-svc -n mern-app-dev
-kubectl apply -f frontend-deployment-dev.yaml
-kubectl apply -f frontend-service-dev.yaml
-kubectl apply -f ingress-resource-dev.yaml
-# echo "192.168.0.215 dev.myexampleapp.com" >> /etc/hosts
